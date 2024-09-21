64af7acae0ebdf8b6939c455fc47089738ccaa03
SNbappy <bappy.just@gmail.com>
2024-09-21 08:31:06 +0600

    created add money button and Add Money Form

--- js/login.js
+++ js/login.js
@@ -4,13 +4,42 @@
 
 
 // step - 1: set event handler
-document.getElementById('button-login').addEventListener('click', function (event) {
+// document.getElementById('button-login').addEventListener('click', function (event) {
 
-    // step-2: prevent default behavior (prevent reloading browser)
-    event.preventDefault();
-    console.log('login button clicked');
+//     // step-2: prevent default behavior (prevent reloading browser)
+//     event.preventDefault();
 
-    // step-3: get the phone number
-    const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
-})
+//     // step-3: get the phone number and the pin number
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber);
+
+//     // step-4: validate phone and pin
+//     // this is not an ideal way. You should not do like this
+//     if (phoneNumber = '5' && pinNumber === '1234') {
+//         console.log('you are logged in');
+//         // step-5: allow user to use the website
+//     }
+//     else {
+//         alert('Wrong phone number or pin');
+//     }
+// })
+
+document.getElementById('button-login')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // get phone number and pin
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(phoneNumber, pinNumber);
+
+        // bad way to validate
+        if (phoneNumber === '5' && pinNumber === '1234') {
+            console.log('you are logged in');
+            window.location.href = '/home.html';
+        }
+        else {
+            alert('Wrong phone number or pin');
+        }
+    })
